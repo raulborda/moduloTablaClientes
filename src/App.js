e9e0419d63f5944727c8725b44e936b7540fc2a8
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-05-15 09:26:03 -0300

    se acomodo archivo App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
-import Tabla from "./components/tabla/Tabla.js";
+import { ConfigProvider } from "antd";
+import "./App.css";
+import GlobalContext from "./components/context/GlobalContext"
+import Tabla from "./components/tabla/Tabla";
+import esES from "antd/lib/locale/es_ES";
 
 function App() {
   return (
     <>
-      <Tabla/>
+    <GlobalContext.Provider>
+      <ConfigProvider
+        locale={esES}
+        theme={{
+          token: {
+            colorPrimary: "#56b43c",
+          },
+        }}
+      >
+        <Tabla />
+      </ConfigProvider>
+    </GlobalContext.Provider>
     </>
   );
 }
