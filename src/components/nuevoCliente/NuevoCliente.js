bc5bc3888a8cfd4d97c67e1e640a58ddbd1b5573
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-05-30 11:40:13 -0300

    se acomodo formulario, se usan valores inciales para la limpieza del form

--- src/components/nuevoCliente/NuevoCliente.js
+++ src/components/nuevoCliente/NuevoCliente.js
@@ -1,19 +1,36 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Button, Divider, Form, Input, Select } from "antd";
 import TextArea from "antd/es/input/TextArea";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 
 const NuevoCliente = () => {
   const URLDOS = process.env.REACT_APP_URL;
 
+  const formRef = useRef(null);
+
   const { idUsu, setIsDrawerVisibleForm } = useContext(GlobalContext);
+
+  const initialValues = {
+    razonSocial: "",
+    descripcion: "",
+    telefono: "",
+    celular: "",
+    cuit: "",
+    email: "",
+    sector: undefined,
+    tipoClientes: undefined,
+    tamano: undefined,
+    zona: undefined,
+    centro: undefined,
+  };
+
 
   const [sector, setSector] = useState(null);
   const [tamaño, setTamaño] = useState(null);
   const [tiposCliente, setTiposClientes] = useState(null);
   const [grupoUno, setGrupoUno] = useState(null);
   const [grupoDos, setGrupoDos] = useState(null);
-
 
   const cargarSector = () => {
     const data = new FormData();
@@ -91,7 +108,6 @@ const NuevoCliente = () => {
   };
 
   useEffect(() => {
-
     cargarSector();
 
     cargarTamaño();
@@ -101,19 +117,37 @@ const NuevoCliente = () => {
     cargarGruUno();
 
     cargarGruDos();
-
   }, []);
 
-//   console.log("sectores: ", sector);
-//   console.log("tamaño: ", tamaño);
-//   console.log("tiposCliente: ", tiposCliente);
-//   console.log("sectores: ", grupoUno);
-//   console.log("sectores: ", grupoDos);
 
-
-  //* FUNCION QUE CARGA LOS DATOS DE UNA NUEVA COSECHA
+  //* FUNCION QUE CARGA LOS DATOS DE UN NUEVO CLIENTE
   function nuevoCli(values) {
-    console.log("Formulario enviado:", values);
+
+    const data = new FormData();
+    data.append("idUsu", idUsu);
+    data.append("razonSocial", values.razonSocial);
+    data.append("descripcion", values.descripcion);
+    data.append("telefono", values.telefono);
+    data.append("celular",  values.celular);
+    data.append("cuit", values.cuit);
+    data.append("email", values.email);
+    data.append("sector", values.sector);
+    data.append("tipoClientes", values.tipoClientes);
+    data.append("tamano", values.tamano);
+    data.append("zona", values.zona);
+    data.append("centro", values.centro);
+    fetch(`${URLDOS}nuevoCliente.php`, {
+      method: "POST",
+      body: data,
+    }).then(function (response) {
+      response.text().then((resp) => {
+        const data = resp;
+        console.log(data);
+      });
+    });
+
+    // Limpiar el formulario
+    formRef.current.setFieldsValue(initialValues);
 
     setIsDrawerVisibleForm(false);
   }
@@ -121,6 +155,8 @@ const NuevoCliente = () => {
   return (
     <>
       <Form
+        ref={formRef}
+        initialValues={initialValues}
         labelCol={{ span: 9 }}
         wrapperCol={{ span: 20 }}
         layout="vertical"
@@ -150,69 +186,79 @@ const NuevoCliente = () => {
           </div>
           <div style={{ flex: 1 }}>
             <Form.Item label="Act. Comercial" name="sector">
-            {sector ? (
-              <Select style={{ width: "170px" }}>
-                {sector.map((sec) => (
-                  <Select.Option key={sec.sec_id} value={sec.sec_id}>
-                    {sec.sec_desc}
-                  </Select.Option>
-                ))}
-              </Select>
-            ):(
-                <Select.Option value="CARGANDO">CARGANDO OPCIONES...</Select.Option>
-            )}
+              {sector ? (
+                <Select style={{ width: "170px" }}>
+                  {sector.map((sec) => (
+                    <Select.Option key={sec.sec_id} value={sec.sec_id}>
+                      {sec.sec_desc}
+                    </Select.Option>
+                  ))}
+                </Select>
+              ) : (
+                <Select.Option value="CARGANDO">
+                  CARGANDO OPCIONES...
+                </Select.Option>
+              )}
             </Form.Item>
             <Form.Item label="Tipo Clientes" name="tipoClientes">
-            {tiposCliente ? (
-              <Select style={{ width: "170px" }}>
-                {tiposCliente.map((tip) => (
-                  <Select.Option key={tip.tip_id} value={tip.tip_id}>
-                    {tip.tip_desc}
-                  </Select.Option>
-                ))}
-              </Select>
-            ):(
-                <Select.Option value="CARGANDO">CARGANDO OPCIONES...</Select.Option>
-            )}
+              {tiposCliente ? (
+                <Select style={{ width: "170px" }}>
+                  {tiposCliente.map((tip) => (
+                    <Select.Option key={tip.tip_id} value={tip.tip_id}>
+                      {tip.tip_desc}
+                    </Select.Option>
+                  ))}
+                </Select>
+              ) : (
+                <Select.Option value="CARGANDO">
+                  CARGANDO OPCIONES...
+                </Select.Option>
+              )}
             </Form.Item>
             <Form.Item label="Tamaño" name="tamano">
-            {tamaño ? (
-              <Select style={{ width: "170px" }}>
-                {tamaño.map((tam) => (
-                  <Select.Option key={tam.tam_id} value={tam.tam_id}>
-                    {tam.tam_desc}
-                  </Select.Option>
-                ))}
-              </Select>
-            ):(
-                <Select.Option value="CARGANDO">CARGANDO OPCIONES...</Select.Option>
-            )}
+              {tamaño ? (
+                <Select style={{ width: "170px" }}>
+                  {tamaño.map((tam) => (
+                    <Select.Option key={tam.tam_id} value={tam.tam_id}>
+                      {tam.tam_desc}
+                    </Select.Option>
+                  ))}
+                </Select>
+              ) : (
+                <Select.Option value="CARGANDO">
+                  CARGANDO OPCIONES...
+                </Select.Option>
+              )}
             </Form.Item>
             <Form.Item label="Zona" name="zona">
-            {grupoUno ? (
-              <Select style={{ width: "170px" }}>
-                {grupoUno.map((uno) => (
-                  <Select.Option key={uno.gruuno_id} value={uno.gruuno_id}>
-                    {uno.gruuno_desc}
-                  </Select.Option>
-                ))}
-              </Select>
-            ):(
-                <Select.Option value="CARGANDO">CARGANDO OPCIONES...</Select.Option>
-            )}
+              {grupoUno ? (
+                <Select style={{ width: "170px" }}>
+                  {grupoUno.map((uno) => (
+                    <Select.Option key={uno.gruuno_id} value={uno.gruuno_id}>
+                      {uno.gruuno_desc}
+                    </Select.Option>
+                  ))}
+                </Select>
+              ) : (
+                <Select.Option value="CARGANDO">
+                  CARGANDO OPCIONES...
+                </Select.Option>
+              )}
             </Form.Item>
             <Form.Item label="Centro" name="centro">
-            {grupoDos ? (
-              <Select style={{ width: "180px" }}>
-                {grupoDos.map((dos) => (
-                  <Select.Option key={dos.grudos_id} value={dos.grudos_id}>
-                    {dos.grudos_desc}
-                  </Select.Option>
-                ))}
-              </Select>
-            ):(
-                <Select.Option value="CARGANDO">CARGANDO OPCIONES...</Select.Option>
-            )}
+              {grupoDos ? (
+                <Select style={{ width: "180px" }}>
+                  {grupoDos.map((dos) => (
+                    <Select.Option key={dos.grudos_id} value={dos.grudos_id}>
+                      {dos.grudos_desc}
+                    </Select.Option>
+                  ))}
+                </Select>
+              ) : (
+                <Select.Option value="CARGANDO">
+                  CARGANDO OPCIONES...
+                </Select.Option>
+              )}
             </Form.Item>
           </div>
         </div>
