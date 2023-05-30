3f3d850a07c1d59884e3364e43a3829024b0c2d4
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2023-05-30 09:54:05 -0300

    se crearon las consultas para llenar todos los selects del formulario

--- src/components/nuevoCliente/NuevoCliente.js
+++ src/components/nuevoCliente/NuevoCliente.js
@@ -1,23 +1,130 @@
 import { Button, Divider, Form, Input, Select } from "antd";
 import TextArea from "antd/es/input/TextArea";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 
 const NuevoCliente = () => {
-  const { setIsDrawerVisibleForm } = useContext(GlobalContext);
+  const URLDOS = process.env.REACT_APP_URL;
 
-  const handleCrearCliente = (values) => {
-    console.log("Formulario enviado:", values);
-    setIsDrawerVisibleForm(false);
+  const { idUsu, setIsDrawerVisibleForm } = useContext(GlobalContext);
+
+  const [sector, setSector] = useState(null);
+  const [tamaño, setTamaño] = useState(null);
+  const [tiposCliente, setTiposClientes] = useState(null);
+  const [grupoUno, setGrupoUno] = useState(null);
+  const [grupoDos, setGrupoDos] = useState(null);
+
+
+  const cargarSector = () => {
+    const data = new FormData();
+    data.append("idU", idUsu);
+    fetch(`${URLDOS}sectorCli.php`, {
+      method: "POST",
+      body: data,
+    }).then(function (response) {
+      response.text().then((resp) => {
+        const data = resp;
+        const objetoData = JSON.parse(data);
+        setSector(objetoData);
+      });
+    });
   };
+
+  const cargarTamaño = () => {
+    const data = new FormData();
+    data.append("idU", idUsu);
+    fetch(`${URLDOS}tamanoCli.php`, {
+      method: "POST",
+      body: data,
+    }).then(function (response) {
+      response.text().then((resp) => {
+        const data = resp;
+        const objetoData = JSON.parse(data);
+        setTamaño(objetoData);
+      });
+    });
+  };
+
+  const cargarTipClientes = () => {
+    const data = new FormData();
+    data.append("idU", idUsu);
+    fetch(`${URLDOS}tiposCli.php`, {
+      method: "POST",
+      body: data,
+    }).then(function (response) {
+      response.text().then((resp) => {
+        const data = resp;
+        const objetoData = JSON.parse(data);
+        setTiposClientes(objetoData);
+      });
+    });
+  };
+
+  const cargarGruUno = () => {
+    const data = new FormData();
+    data.append("idU", idUsu);
+    fetch(`${URLDOS}gruUnoCli.php`, {
+      method: "POST",
+      body: data,
+    }).then(function (response) {
+      response.text().then((resp) => {
+        const data = resp;
+        const objetoData = JSON.parse(data);
+        setGrupoUno(objetoData);
+      });
+    });
+  };
+
+  const cargarGruDos = () => {
+    const data = new FormData();
+    data.append("idU", idUsu);
+    fetch(`${URLDOS}gruDosCli.php`, {
+      method: "POST",
+      body: data,
+    }).then(function (response) {
+      response.text().then((resp) => {
+        const data = resp;
+        const objetoData = JSON.parse(data);
+        setGrupoDos(objetoData);
+      });
+    });
+  };
+
+  useEffect(() => {
+
+    cargarSector();
+
+    cargarTamaño();
+
+    cargarTipClientes();
+
+    cargarGruUno();
+
+    cargarGruDos();
+
+  }, []);
+
+//   console.log("sectores: ", sector);
+//   console.log("tamaño: ", tamaño);
+//   console.log("tiposCliente: ", tiposCliente);
+//   console.log("sectores: ", grupoUno);
+//   console.log("sectores: ", grupoDos);
+
+
+  //* FUNCION QUE CARGA LOS DATOS DE UNA NUEVA COSECHA
+  function nuevoCli(values) {
+    console.log("Formulario enviado:", values);
+
+    setIsDrawerVisibleForm(false);
+  }
 
   return (
     <>
       <Form
-        labelCol={{ span: 8 }}
+        labelCol={{ span: 9 }}
         wrapperCol={{ span: 20 }}
         layout="vertical"
-        onFinish={handleCrearCliente}
+        onFinish={nuevoCli}
         style={{ marginLeft: "35px" }}
       >
         <div style={{ display: "flex" }}>
@@ -42,30 +149,70 @@ const NuevoCliente = () => {
             </Form.Item>
           </div>
           <div style={{ flex: 1 }}>
-            <Form.Item label="Sector" name="sector">
+            <Form.Item label="Act. Comercial" name="sector">
+            {sector ? (
               <Select style={{ width: "170px" }}>
-                <Select.Option value="AGRICULTURA">AGRICULTURA</Select.Option>
+                {sector.map((sec) => (
+                  <Select.Option key={sec.sec_id} value={sec.sec_id}>
+                    {sec.sec_desc}
+                  </Select.Option>
+                ))}
               </Select>
+            ):(
+                <Select.Option value="CARGANDO">CARGANDO OPCIONES...</Select.Option>
+            )}
             </Form.Item>
             <Form.Item label="Tipo Clientes" name="tipoClientes">
+            {tiposCliente ? (
               <Select style={{ width: "170px" }}>
-                <Select.Option value="NO SOCIO">NO SOCIO</Select.Option>
+                {tiposCliente.map((tip) => (
+                  <Select.Option key={tip.tip_id} value={tip.tip_id}>
+                    {tip.tip_desc}
+                  </Select.Option>
+                ))}
               </Select>
+            ):(
+                <Select.Option value="CARGANDO">CARGANDO OPCIONES...</Select.Option>
+            )}
             </Form.Item>
             <Form.Item label="Tamaño" name="tamano">
+            {tamaño ? (
               <Select style={{ width: "170px" }}>
-                <Select.Option value="MEDIANO">MEDIANO</Select.Option>
+                {tamaño.map((tam) => (
+                  <Select.Option key={tam.tam_id} value={tam.tam_id}>
+                    {tam.tam_desc}
+                  </Select.Option>
+                ))}
               </Select>
+            ):(
+                <Select.Option value="CARGANDO">CARGANDO OPCIONES...</Select.Option>
+            )}
             </Form.Item>
             <Form.Item label="Zona" name="zona">
+            {grupoUno ? (
               <Select style={{ width: "170px" }}>
-                <Select.Option value="SANTA ROSA">SANTA ROSA</Select.Option>
+                {grupoUno.map((uno) => (
+                  <Select.Option key={uno.gruuno_id} value={uno.gruuno_id}>
+                    {uno.gruuno_desc}
+                  </Select.Option>
+                ))}
               </Select>
+            ):(
+                <Select.Option value="CARGANDO">CARGANDO OPCIONES...</Select.Option>
+            )}
             </Form.Item>
             <Form.Item label="Centro" name="centro">
-              <Select style={{ width: "170px" }}>
-                <Select.Option value="TERCEROS">TERCEROS</Select.Option>
+            {grupoDos ? (
+              <Select style={{ width: "180px" }}>
+                {grupoDos.map((dos) => (
+                  <Select.Option key={dos.grudos_id} value={dos.grudos_id}>
+                    {dos.grudos_desc}
+                  </Select.Option>
+                ))}
               </Select>
+            ):(
+                <Select.Option value="CARGANDO">CARGANDO OPCIONES...</Select.Option>
+            )}
             </Form.Item>
           </div>
         </div>
